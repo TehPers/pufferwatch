56da54b36089fdacbb742f2ad4c95807354e7c69
TehPers <tehperz@gmail.com>
2022-05-15 23:11:29 -0700

    Replace let _ with drop to make clippy happy

--- src/widgets/root.rs
+++ src/widgets/root.rs
@@ -208,9 +208,9 @@ impl<'i> State for RootState<'i> {
         if let Some((command_input_state, stdin)) = self.command_input_state.as_mut() {
             for cmd in command_input_state.take_submitted() {
                 debug!(?cmd, "sending command");
-                let _ = stdin.write_all(&cmd);
-                let _ = stdin.write_all("\n");
-                let _ = stdin.flush();
+                drop(stdin.write_all(&cmd));
+                drop(stdin.write_all("\n"));
+                drop(stdin.flush());
             }
         }
 
